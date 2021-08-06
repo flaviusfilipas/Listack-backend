fdf65f2351fb9f49bbf5a85583932934bb2419e4
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-06 17:04:06 +0300

    Added functionality for updating a task.

--- src/main/java/com/backend/listack/resource/TaskResource.java
+++ src/main/java/com/backend/listack/resource/TaskResource.java
@@ -3,20 +3,28 @@ package com.backend.listack.resource;
 import com.backend.listack.dto.TaskDTO;
 import com.backend.listack.service.TaskService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping("/api/tasks")
 public class TaskResource {
     private final TaskService taskService;
 
     @PostMapping
     public ResponseEntity<TaskDTO> save(@RequestBody TaskDTO taskDTO){
+        TaskDTO save = taskService.save(taskDTO);
+        return ResponseEntity.ok(save);
+    }
+    @PutMapping
+    public ResponseEntity<TaskDTO> update(@RequestBody TaskDTO taskDTO){
+        if(taskDTO.getId() == null){
+            log.error("Could not updated a task without an id");
+            return ResponseEntity.badRequest().build();
+        }
         TaskDTO save = taskService.save(taskDTO);
         return ResponseEntity.ok(save);
     }
