799374e74f7b0a963d6b8167593774d0eaf38084
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-06 09:06:11 +0300

    Init

---
+++ src/main/java/com/backend/listack/ListackApplication.java
@@ -0,0 +1,13 @@
+package com.backend.listack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/listack/ListackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.listack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
