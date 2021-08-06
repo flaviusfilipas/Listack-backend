e692a936d654752cbc3f050cbac8cc36e25195f1
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-08-06 16:55:14 +0300

    Updated task mapper.

--- src/main/java/com/backend/listack/mapper/TaskMapper.java
+++ src/main/java/com/backend/listack/mapper/TaskMapper.java
@@ -3,9 +3,12 @@ package com.backend.listack.mapper;
 import com.backend.listack.dto.TaskDTO;
 import com.backend.listack.entity.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
+    @Mapping(source = "list.id", target = "listId")
     TaskDTO toDTO(Task task);
+    @Mapping(source = "listId", target = "list.id")
     Task toEntity(TaskDTO taskDTO);
 }
